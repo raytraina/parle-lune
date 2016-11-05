e9a2da9a1e4d0158f608eb5a2f464544aa7315b2
Rachel Traina-Grandon <racheltrainagrandon@gmail.com>
2016-11-04 18:31:10 -0700

    Added initial setup for app. Bootstrap, installed necessary modules, setup server.

---
+++ server.js
@@ -0,0 +1,29 @@
+// Express initializes app to be a function handler that you can supply to http server instances
+var app = require('express')();
+var http = require('http').Server(app);
+// Initialize a new instance of socket.io by passing http server object
+var io = require('socket.io')(http);
+var path = require('path');
+ 
+// Initializes application routing, defines route handler at '/'
+app.get('/', function(req, res){
+  var express=require('express');
+  app.use(express.static(path.join(__dirname)));
+  res.sendFile(path.join(__dirname, '../chatner', './templates/index.html'));
+});
+
+// Online users list // Not currently used, here for future versions
+var usernames = {};
+
+// Registers events on socket connection and emits an event to connected clients
+io.on('connection', function(socket){ 
+  console.log('A user has connected\n');
+});
+ 
+// Attaches server to engine.io instance bound to port 3000
+// Listens for incoming sockets on port 3000
+var server = http.listen(3000, function(){
+  console.log('listening on port 3000\n');
+});
+
+module.exports = server;
